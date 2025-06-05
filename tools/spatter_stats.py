a704bf62296478ae7e5836fda0ba72377eed0a0d
Connor Radelja <cradelja3@gatech.edu>
2025-06-05 11:10:07 -0700

    Updated spatter-stats.py to report double the bandwidth for GS patterns

--- tools/spatter_stats.py
+++ tools/spatter_stats.py
@@ -1,7 +1,8 @@
 import sys
 import pandas as pd
+import json
 
-def load_data(filename: str) -> pd.DataFrame:
+def load_stats(filename: str) -> pd.DataFrame:
     df = pd.DataFrame()
 
     try:
@@ -15,18 +16,32 @@ def load_data(filename: str) -> pd.DataFrame:
 
     return df
 
-def print_stats(df: pd.DataFrame):
+def get_kernel_data(filename: str) -> list[str]:
+    try:
+        with open(filename, 'r') as file:
+            patterns = json.load(file)
+    except Exception as error:
+        print(error)
+        sys.exit(1)
+
+    kernel_data = []
+    for pattern in patterns:
+        kernel_data.append(pattern['kernel'].lower())
+
+    return kernel_data
+
+def print_stats(stats_data: pd.DataFrame, kernel_data: list[str]):
     config = 0
     prev_time = 0.0
 
     print(f"{'config':<15}{'bytes':<15}{'time(s)':<15}{'bw(MB/s)':<15}{'cycles':<15}")
 
-    for row in df.itertuples():
+    for row in stats_data.itertuples():
         stat_name = row.StatisticName.strip()
         stat_value = row._asdict()['_7']
 
         if stat_name == 'total_bytes_write':
-            bytes = stat_value
+            bytes = (stat_value * 2) if kernel_data[config] == 'gs' else stat_value
 
         elif stat_name == 'cycles':
             curr_time = row.SimTime / 1e+12
@@ -41,10 +56,14 @@ def print_stats(df: pd.DataFrame):
             prev_time = curr_time
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f'Usage: python3 {sys.argv[0]} <filename>')
+    if len(sys.argv) != 3:
+        print(f'Usage: python3 {sys.argv[0]} <stats.csv> <trace.json>')
         sys.exit(1)
 
-    filename = sys.argv[1]
-    df = load_data(filename)
-    print_stats(df)
+    stats_file = sys.argv[1]
+    trace_file = sys.argv[2]
+
+    stats_data = load_stats(stats_file)
+    kernel_data = get_kernel_data(trace_file)
+
+    print_stats(stats_data, kernel_data)
